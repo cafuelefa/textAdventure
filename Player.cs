bb18e7d416576c562b24191fcf1a2cb392be2707
Jesse <jessegriffith36@gmail.com>
2019-10-06 12:18:54 -0500

    1

---
+++ Player.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace textAdventure
+{
+    class Player
+    {
+        //stats
+        public float maxHp;
+        public float maxMana;
+        public float maxEncumbrance;
+        public float hp;
+        public float mana;
+        public float encumbrance;
+        public float str;
+        public float wis;
+        public float agi;
+        public Random rnd = new Random();
+        public String archetype;
+        public String name;
+
+        //inventory
+        protected List<Item> inv;
+
+        public Player(String n, String a)
+        {
+            archetype = a;
+            name = n;
+            Wake();
+        }
+
+        public void Wake()
+        {
+            switch (archetype)
+            {
+                case "bruiser":
+                    inv = new List<Item>();
+                    inv.Add(new Axe(1));
+                    maxHp = rnd.Next(120, 135);
+                    maxMana = rnd.Next(50, 60);
+                    maxEncumbrance = rnd.Next(120, 135);
+                    hp = maxHp;
+                    mana = maxMana;
+                    str = rnd.Next(8, 10);
+                    wis = rnd.Next(1, 4);
+                    agi = rnd.Next(5, 7);
+                    // need to add formula to determine encumbrance
+                    break;
+
+                case "clairvoyant":
+                    inv = new List<Item>();
+                    inv.Add(new Axe(1));
+                    maxHp = rnd.Next(120, 135);
+                    maxMana = rnd.Next(50, 60);
+                    maxEncumbrance = rnd.Next(120, 135);
+                    hp = maxHp;
+                    mana = maxMana;
+                    str = rnd.Next(8, 10);
+                    wis = rnd.Next(1, 4);
+                    agi = rnd.Next(5, 7);
+                    // need to add formula to determine encumbrance
+                    break;
+
+                case "bowman":
+                    inv = new List<Item>();
+                    inv.Add(new Axe(1));
+                    maxHp = rnd.Next(120, 135);
+                    maxMana = rnd.Next(50, 60);
+                    maxEncumbrance = rnd.Next(120, 135);
+                    hp = maxHp;
+                    mana = maxMana;
+                    str = rnd.Next(8, 10);
+                    wis = rnd.Next(1, 4);
+                    agi = rnd.Next(5, 7);
+                    // need to add formula to determine encumbrance
+                    break;
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace textAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initiate variables
+            String input = "";
+            String arch = "";
+            //parser
+            Scripter narrator = new Scripter();
+
+        StartGame:
+            narrator.NewLine("Welcome to the magical world of Text Adventure!");
+            narrator.NewLine("What is your name?");
+            centerText(narrator.script);
+            narrator.Clear();
+            input = Console.ReadLine();
+            String name = input;
+
+            bool failedClass = false;
+            narrator.NewLine( name + ", huh... Sure. That'll work.");
+            narrator.NewLine("And how do you fancy yourself? A Brute? A Wizard? A Bowman, perhaps?");
+            FailedClass:
+                if (failedClass)
+                {
+                narrator.NewLine("Try again.");
+                }
+            centerText(narrator.script);
+            narrator.Clear();
+            input = Console.ReadLine().ToLower();
+            if ( input == "brute" || input == "clairvoyant" || input == "bowman" )
+            {
+                failedClass = false;
+                arch = input;
+            }
+            else
+            {
+                failedClass = true;
+                goto FailedClass;
+            }
+
+            Player me = new Player(name, arch);
+
+            narrator.NewLine("And so, the journey of " + me.name + " the " + me.archetype + " begins...");
+            centerText(narrator.script);
+            narrator.Clear();
+        }
+        private static void centerText(List<String> text)
+        {
+            foreach (String element in text)
+            {
+                Console.Write(new string(' ', (Console.WindowWidth - element.Length) / 2));
+                Console.WriteLine(element);
+            }
+        }
+        public class Scripter
+        {
+            public List<String> script = new List<String>();
+            public  void NewLine( String line )
+            {
+                script.Add(line);
+            }
+            public void Clear()
+            {
+                script.Clear();
+            }
+        }
+    }
+}
